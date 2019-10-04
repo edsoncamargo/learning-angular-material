b0219a756c39f4c450782b6d76d79037bd5f4063
Edson Camargo Menezes <dinhocmenezes@hotmail.com>
2019-10-04 14:12:38 -0300

    My first commit
    Learning angular material, adding modules and testing.

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+
+import {
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatIconModule,
+  MatBadgeModule,
+  MatProgressSpinnerModule,
+  MatToolbarModule
+} from '@angular/material';
+
+const material = [
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatIconModule,
+  MatBadgeModule,
+  MatProgressSpinnerModule,
+  MatToolbarModule
+]
+
+@NgModule({
+  imports: [material],
+  exports: [material]
+})
+
+export class MaterialModule { }
